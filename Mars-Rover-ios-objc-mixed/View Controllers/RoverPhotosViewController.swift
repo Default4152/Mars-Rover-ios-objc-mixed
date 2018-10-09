8b4234a15a915276f3bb3f8cd066df89767154be
Conner Alegre <cga0@pm.me>
2018-10-09 15:10:11 -0600

    fix issue with bridging header moving into another folder

---
+++ Mars-Rover-ios-objc-mixed/View Controllers/RoverPhotosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoverPhotosViewController.swift
+//  Mars-Rover-ios-objc-mixed
+//
+//  Created by Conner on 10/9/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class RoverPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
